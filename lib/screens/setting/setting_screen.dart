84eda55be81b7c3887bf509c97257860f0f20993
opiasdream <opiasdream@protonmail.com>
2024-02-19 02:15:40 +0300

    added in app review

--- lib/screens/setting/setting_screen.dart
+++ lib/screens/setting/setting_screen.dart
@@ -78,9 +78,7 @@ class SettingScreen extends StatelessWidget {
       onTap: () async {
         try {
           var inAppReview = InAppReview.instance;
-
           Future<bool> isAvailable = inAppReview.isAvailable();
-
           isAvailable.then((result) async {
             if (result) {
               inAppReview.requestReview();
