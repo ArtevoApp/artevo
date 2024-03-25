d5ab9d3edc58c1ced5767715aff232e8d912f07e
opiasdream <opiasdream@protonmail.com>
2024-03-25 07:27:24 +0300

    painting zoom dialog added

--- lib/features/painting/views/widgets/painting_zoom_dialog.dart
+++ lib/features/painting/views/widgets/painting_zoom_dialog.dart
@@ -1,10 +1,11 @@
 import 'package:artevo/common/config/color_schemes.dart';
 import 'package:artevo/common/constants/dimens.dart';
 import 'package:artevo/common/constants/text_styles.dart';
-import 'package:artevo/common/widgets/add_bookmark_button.dart';
+import 'package:artevo/common/widgets/bookmarking_button.dart';
 import 'package:artevo/common/widgets/image_viewer.dart';
 import 'package:artevo_package/models/painting_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:widget_zoom/widget_zoom.dart';
 
 class PaintingZoomDialog extends StatelessWidget {
@@ -24,7 +25,7 @@ class PaintingZoomDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       contentPadding: EdgeInsets.all(0),
-      insetPadding: EdgeInsets.all(16),
+      insetPadding: EdgeInsets.all(largePadding),
       scrollable: true,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       content: SizedBox(
@@ -35,7 +36,7 @@ class PaintingZoomDialog extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 WidgetZoom(
-                  heroAnimationTag: 'tag',
+                  heroAnimationTag: 'paintingZoom',
                   zoomWidget: ImageViewer(
                       url: painting.imageUrl,
                       boxFit: BoxFit.contain,
@@ -44,9 +45,20 @@ class PaintingZoomDialog extends StatelessWidget {
                 Container(
                   width: columnWidth,
                   padding: EdgeInsets.all(defaultPadding),
+                  decoration: BoxDecoration(
+                    color: darkColorScheme.background,
+                    border: Border(
+                      top: BorderSide(
+                          color: lightColorScheme.background, width: .5),
+                    ),
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(smallPadding),
+                    ),
+                  ),
                   child: Text(
                       "${painting.title} by ${painting.creator}, ${painting.year}",
-                      style: TextStyles.info,
+                      style: TextStyles.info
+                          .copyWith(color: lightColorScheme.background),
                       textAlign: TextAlign.center),
                 ),
               ],
@@ -56,7 +68,7 @@ class PaintingZoomDialog extends StatelessWidget {
               right: smallPadding,
               child: CircleAvatar(
                   radius: smallIconSize,
-                  child: AddBookmarkButton(
+                  child: BookmarkingButton(
                     size: hugeIconSize,
                     iconSize: defaultIconSize,
                     color: lightColorScheme.background,
@@ -75,6 +87,6 @@ class PaintingZoomDialog extends StatelessWidget {
           ],
         ),
       ),
-    );
+    ).animate().fadeIn(duration: 300.milliseconds);
   }
 }
