362bf143ca31710d0d6b3c9622a450d3620b389c
opiasdream <opiasdream@protonmail.com>
2024-03-25 07:35:28 +0300

    peom feat. added as draft

---
+++ lib/features/poem/view/poem_discover_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// TODO:
+class PoemDiscoverView extends StatelessWidget {
+  const PoemDiscoverView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:
+          Text("Bu sayfa henüz hazır değil\n\n\nThis page is not yet ready"),
+    );
+  }
+}
