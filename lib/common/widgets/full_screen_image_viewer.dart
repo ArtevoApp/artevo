52451a662b03e81b44b39f69372850f6ec544d02
opiasdream <opiasdream@protonmail.com>
2024-09-29 01:54:55 +0300

    added full screen image viewer

---
+++ lib/common/widgets/full_screen_image_viewer.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FullScreenImageViewer extends StatefulWidget {
+  final Widget image;
+  final double minScale;
+  final double maxScale;
+  final Object heroAnimationTag;
+  final double? fullScreenDoubleTapZoomScale;
+  const FullScreenImageViewer({
+    Key? key,
+    required this.image,
+    this.minScale = 1.0,
+    this.maxScale = 4.0,
+    required this.heroAnimationTag,
+    this.fullScreenDoubleTapZoomScale = 2.0,
+  }) : super(key: key);
+
+  static void open({
+    required BuildContext context,
+    required Widget image,
+    required String heroAnimationTag,
+    double minScale = 1.0,
+    double maxScale = 4.0,
+    double fullScreenDoubleTapZoomScale = 2.0,
+  }) async {
+    await Navigator.push(
+      context,
+      PageRouteBuilder(
+        opaque: false,
+        pageBuilder: (context, animation1, animation2) => FadeTransition(
+          opacity: animation1,
+          child: FullScreenImageViewer(
+            image: image,
+            heroAnimationTag: heroAnimationTag,
+            minScale: minScale,
+            maxScale: maxScale,
+            fullScreenDoubleTapZoomScale: fullScreenDoubleTapZoomScale,
+          ),
+        ),
+        transitionDuration: const Duration(milliseconds: 300),
+        reverseTransitionDuration: const Duration(milliseconds: 300),
+      ),
+    );
+  }
+
+  @override
+  State<FullScreenImageViewer> createState() => _ImageZoomFullscreenState();
+}
+
+class _ImageZoomFullscreenState extends State<FullScreenImageViewer>
+    with SingleTickerProviderStateMixin {
+  final TransformationController _transformationController =
+      TransformationController();
+  late AnimationController _animationController;
+  late double closingTreshold = MediaQuery.of(context).size.height /
+      5; //the higher you set the last value the earlier the full screen gets closed
+
+  Animation<Matrix4>? _animation;
+  double _opacity = 1;
+  double _imagePosition = 0;
+  Duration _animationDuration = Duration.zero;
+  Duration _opacityDuration = Duration.zero;
+  late double _currentScale = widget.minScale;
+  TapDownDetails? _doubleTapDownDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..addListener(() => _transformationController.value = _animation!.value);
+  }
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: AnimatedOpacity(
+            duration: _opacityDuration,
+            opacity: _opacity,
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        AnimatedPositioned(
+          duration: _animationDuration,
+          top: _imagePosition,
+          bottom: -_imagePosition,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: InteractiveViewer(
+              constrained: true,
+              transformationController: _transformationController,
+              minScale: widget.minScale,
+              maxScale: widget.maxScale,
+              onInteractionStart: _onInteractionStart,
+              onInteractionUpdate: _onInteractionUpdate,
+              onInteractionEnd: _onInteractionEnd,
+              child: GestureDetector(
+                // need to have both methods, otherwise the zoom will be triggered before the second tap releases the screen
+                onDoubleTapDown: (details) => _doubleTapDownDetails = details,
+                onDoubleTap: _zoomInOut,
+                child: Hero(
+                  tag: widget.heroAnimationTag,
+                  child: widget.image,
+                ),
+              ),
+            ),
+          ),
+        ),
+        SafeArea(
+          child: Align(
+            alignment: Alignment.topRight,
+            child: GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () => Navigator.of(context).pop(),
+              child: AnimatedOpacity(
+                duration: _opacityDuration,
+                opacity: _opacity,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  child: Icon(
+                    CupertinoIcons.xmark,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _zoomInOut() {
+    final Offset tapPosition = _doubleTapDownDetails!.localPosition;
+    final double zoomScale =
+        widget.fullScreenDoubleTapZoomScale ?? widget.maxScale;
+
+    final double x = -tapPosition.dx * (zoomScale - 1);
+    final double y = -tapPosition.dy * (zoomScale - 1);
+
+    final Matrix4 zoomedMatrix = Matrix4.identity()
+      ..translate(x, y)
+      ..scale(zoomScale);
+
+    final Matrix4 widgetMatrix = _transformationController.value.isIdentity()
+        ? zoomedMatrix
+        : Matrix4.identity();
+
+    _animation = Matrix4Tween(
+      begin: _transformationController.value,
+      end: widgetMatrix,
+    ).animate(
+      CurveTween(curve: Curves.easeOut).animate(_animationController),
+    );
+
+    _animationController.forward(from: 0);
+    _currentScale = _transformationController.value.isIdentity()
+        ? zoomScale
+        : widget.minScale;
+  }
+
+  void _onInteractionStart(ScaleStartDetails details) {
+    _animationDuration = Duration.zero;
+    _opacityDuration = Duration.zero;
+  }
+
+  void _onInteractionEnd(ScaleEndDetails details) async {
+    _currentScale = _transformationController.value.getMaxScaleOnAxis();
+    setState(() {
+      _animationDuration = const Duration(milliseconds: 300);
+    });
+    if (_imagePosition > closingTreshold) {
+      setState(() {
+        _imagePosition = MediaQuery.of(context).size.height; // move image down
+      });
+      Navigator.of(context).pop();
+    } else {
+      setState(() {
+        _imagePosition = 0;
+        _opacity = 1;
+        _opacityDuration = const Duration(milliseconds: 300);
+      });
+    }
+  }
+
+  void _onInteractionUpdate(ScaleUpdateDetails details) async {
+    // chose 1.05 because maybe the image was not fully zoomed back but it almost looks like that
+    if (details.pointerCount == 1 && _currentScale <= 1.05) {
+      setState(() {
+        _imagePosition += details.focalPointDelta.dy;
+        _opacity =
+            (1 - (_imagePosition / closingTreshold)).clamp(0, 1).toDouble();
+      });
+    }
+  }
+}
