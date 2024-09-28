cc70e571487478b0aaca3b9319f755059ccfb8d4
opiasdream <opiasdream@protonmail.com>
2024-09-29 00:33:04 +0300

    removed rating feature

--- lib/services/database/firebase/realtime_service.dart
+++ lib/services/database/firebase/realtime_service.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
-import '../../apis/ip_address_service.dart';
 import 'package:artevo_package/models/version_data.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:uuid/uuid.dart';
 
 class RealtimeService {
   final _firebase = FirebaseDatabase.instance;
@@ -57,28 +55,6 @@ class RealtimeService {
       }
     } catch (e) {
       return null;
-    }
-  }
-
-  Future<void> sendPollFeedBack(
-      {required String date,
-      required String comment,
-      required double rating}) async {
-    try {
-      final String uuid = const Uuid().v4();
-
-      final Map<String, dynamic> data = {
-        "uuid": uuid,
-        "date": date,
-        "comment": comment,
-        "rating": rating,
-        "ip":
-            await IpAddressService.getIpAddress.then((value) => value?.toMap()),
-      };
-
-      await _firebase.ref("poll/$date/$uuid").set(data);
-    } catch (e) {
-      return;
     }
   }
 }
